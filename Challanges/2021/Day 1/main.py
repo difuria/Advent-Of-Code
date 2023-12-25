67dc8dfae795c1cc401d7eddd4eb567cb6eedeb1
difuria <69213222+difuria@users.noreply.github.com>
2023-12-25 08:42:23 +0000

    Update main.py with task 2 solution

--- Challanges/2021/Day 1/main.py
+++ Challanges/2021/Day 1/main.py
@@ -1,21 +1,36 @@
 import os, sys
 
-input_text = "test_report_1.txt"
+input_text = "puzzle_report_1.txt"
 
 class SonarSweep:
     def __init__(self, report):
         self.report = list(map(int, report.strip().split("\n")))
-    
-    def number_of_increases(self):
-        # Solution to task 1
+
+    def __count_increases(self, report):
         count = 0
-        for index, line in enumerate(self.report):
+        for index, line in enumerate(report):
             if index == 0:
                 continue
-            elif self.report[index-1] < line:
+            elif report[index-1] < line:
                 count += 1
         
         print(f"Increased {count} times.")
+
+    def number_of_increases(self):
+        # Solution to task 1
+        self.__count_increases(self.report)
+
+    def number_of_sliding_increases(self, sliding_window=3):
+        # Solution to task 2
+        sliding_report = []
+        
+        for index, line in enumerate(self.report):
+            end_index = index+sliding_window
+            if end_index <= len(self.report):
+                sliding_window_sum = sum(self.report[index:end_index])
+                sliding_report.append(sliding_window_sum)
+        
+        self.__count_increases(sliding_report)
 
 if __name__ == "__main__":
     path = os.path.dirname(__file__)
@@ -31,3 +46,4 @@ if __name__ == "__main__":
 
     sonar_sweep = SonarSweep(report)
     sonar_sweep.number_of_increases()
+    sonar_sweep.number_of_sliding_increases()
