8fa2a42f887e104fb6cef7dd55281d564e2aefbd
difuria <69213222+difuria@users.noreply.github.com>
2023-12-28 22:04:33 +0000

    Add task 2 solution

--- Challanges/2023/Day 8/main.py
+++ Challanges/2023/Day 8/main.py
@@ -1,6 +1,7 @@
-import os, re, sys
+import math, os, re, sys
 
 input_text_task_1 = "puzzle_path_1.txt"
+input_text_task_2 = "puzzle_path_1.txt"
 
 class HauntedWasteland:
     def __init__(self):
@@ -16,8 +17,8 @@ class HauntedWasteland:
             left, right = re.sub(r"(\(|\))", "", LR).split(",")
             self.path_directions[key.strip()] = { "L": left.strip(), "R": right.strip()}
 
-    def run_path(self, start = "AAA", end = "ZZZ"):
-        # Solution to task 1
+    def run_path(self, start = "AAA", end = "ZZZ", output = True):
+        # Solution to task 1 (Also used for task 2)
         steps = 0
         current_location = start
         direction_index = 0
@@ -26,13 +27,28 @@ class HauntedWasteland:
             current_location = self.path_directions[current_location][direction]
             steps += 1
 
-            if current_location == end:
-                print(f"Steps taken {steps}.")
-                return
+            if current_location[-len(end):] == end:
+                if output:
+                    print(f"Steps taken for {start} to {end} was {steps} steps.")
+                return steps
 
             direction_index += 1
             if direction_index >= len(self.directions):
                 direction_index = 0
+    
+    def run_ghost_path(self, start = "A", end = "Z"):
+        # Solution to task 2
+        ghost_current_location = []
+
+        for path in self.path_directions:
+            if path[-len(start)] == start:
+                ghost_current_location.append(path)
+        
+        steps = []
+        for ghost in ghost_current_location:
+            steps.append(self.run_path(ghost, end, False))
+        
+        print(f"Ghost steps taken for {start} to {end} was {math.lcm(*steps)} steps.")
 
 def get_file(task, path, file):
     file = os.path.join(path, f"Task {task} Inputs", file)
@@ -52,3 +68,6 @@ if __name__ == "__main__":
     haunted_wasteland = HauntedWasteland()
     haunted_wasteland.extract_path_info(get_file(1, path, input_text_task_1))
     haunted_wasteland.run_path()
+
+    haunted_wasteland.extract_path_info(get_file(2, path, input_text_task_2))
+    haunted_wasteland.run_ghost_path()
