5de9e9d1c4dae6757b0f06971a660d95e1bee50c
difuria <69213222+difuria@users.noreply.github.com>
2024-01-04 14:16:25 +0000

    Refactor to work for task 2

--- Challanges/2023/Day 11/main.py
+++ Challanges/2023/Day 11/main.py
@@ -7,6 +7,9 @@ class CosmicExpansion:
         self.original_image = image
         self.galaxy_locations = []
 
+        self.empty_x = set()
+        self.empty_y = set()
+
         self.load_image()
 
     def load_image(self, image = None):
@@ -17,45 +20,51 @@ class CosmicExpansion:
             split_image = image.strip().split("\n")
 
         self.image = [list(row) for row in split_image]
-        self.adjusted_image = [list(row) for row in split_image]
 
     def check_image(self):
         self.galaxy_locations = []
-        no_galaxies_in_rows = []
+        self.empty_x = set()
         for i, row in enumerate(self.image):
-            if "#" not in row:
-                no_galaxies_in_rows.append(i)
+            if not "#" in row:
+                self.empty_y.add(i)
 
-        no_galaxies_in_cols = []
-        for i, col in enumerate(self.image[0]):
-            no_galaxies_in_cols.append(i)
+        for i in range(len(self.image[0])):
+            self.empty_x.add(i)
             for j in range(len(self.image)):
                 if self.image[j][i] == "#":
-                    no_galaxies_in_cols.pop()
+                    self.empty_x.remove(i)
                     break
-
-        for i, row in enumerate(no_galaxies_in_rows):
-            self.adjusted_image.insert(row + i, ["."] * len(self.image[0]))
-        
-        for i, col in enumerate(no_galaxies_in_cols):
-            for j in range(len(self.adjusted_image)):
-                self.adjusted_image[j].insert(col + i, ".")
 
         # Last get the galaxy locations
         galaxy = 0
-        for y in range(len(self.adjusted_image)):
-            for x in range(len(self.adjusted_image[y])):
-                if self.adjusted_image[y][x] == "#":
+        for y in range(len(self.image)):
+            for x in range(len(self.image[y])):
+                if self.image[y][x] == "#":
                     self.galaxy_locations.append([x,y])
                     galaxy += 1
 
-    def gather_shortest_locations(self):
+    def gather_shortest_locations(self, gap_distance = 2):
         distance = 0
         for i in range(len(self.galaxy_locations)-1):
             for j in range(i+1, len(self.galaxy_locations)):
                 x_f_galaxy, y_f_galaxy = self.galaxy_locations[i]
                 x_s_galaxy, y_s_galaxy = self.galaxy_locations[j]
-                distance += abs(x_f_galaxy - x_s_galaxy) + abs(y_f_galaxy - y_s_galaxy)
+
+                x_max = max(x_f_galaxy, x_s_galaxy)
+                x_min = min(x_f_galaxy, x_s_galaxy)
+                y_max = max(y_f_galaxy, y_s_galaxy)
+                y_min = min(y_f_galaxy, y_s_galaxy)
+
+                extra_rows_and_columns = 0
+                for x in range(x_min, x_max):
+                    if x in self.empty_x:
+                        extra_rows_and_columns += gap_distance - 1
+
+                for y in range(y_min, y_max):
+                    if y in self.empty_y:
+                        extra_rows_and_columns += gap_distance - 1       
+
+                distance += (x_max - x_min) + (y_max - y_min) + extra_rows_and_columns
         
         print(f"Distance {distance}")
 
@@ -77,3 +86,6 @@ if __name__ == "__main__":
     cosmic_expansion = CosmicExpansion(get_file(path, input_text))
     cosmic_expansion.check_image()
     cosmic_expansion.gather_shortest_locations()
+
+    cosmic_expansion.check_image()
+    cosmic_expansion.gather_shortest_locations(1000000)
