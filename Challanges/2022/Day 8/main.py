c55ec887f7ba2d326e367505fad6de6048a217d9
difuria <69213222+difuria@users.noreply.github.com>
2024-01-16 09:44:19 +0000

    Add task 2 solution

--- Challanges/2022/Day 8/main.py
+++ Challanges/2022/Day 8/main.py
@@ -4,8 +4,9 @@ input_text = "puzzle_tree_heights_1.txt"
 
 class Treetops:
     def __init__(self, trees):
-        self.trees = [list(ts) for ts in trees.strip().splitlines()]
-        self.seen_trees = [list(ts) for ts in trees.strip().splitlines()]
+        self.trees = [list(map(int, ts)) for ts in trees.strip().splitlines()]
+        self.seen_trees = [list(map(int, ts)) for ts in trees.strip().splitlines()]
+        self.scenic_scores = [[0][:] * len(ts) for ts in trees.strip().splitlines()]
 
     def find_visible_trees(self):
         visible = 0
@@ -64,10 +65,56 @@ class Treetops:
                                 self.seen_trees[i][new_x] = "X"
 
         for row in self.seen_trees:
-            print("".join(row))
+            print(row)
         print()
 
         print(f"There were {visible} trees.")
+    
+    def find_scenic_score(self):
+        views = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        max_scenic_score = 0
+        for y in range(len(self.trees)):
+            for x in range(len(self.trees[y])):
+                scenic_score = 0
+                for view in views:
+                    view_scenic_score = 0
+                    current_x = x
+                    current_y = y
+                    max_tree = -1
+                    move_x, move_y = view
+
+                    if move_x != 0:
+                        for j in range(1, len(self.trees[y])):
+                            current_x += move_x
+                            if current_x < 0 or current_x >= len(self.trees[y]):
+                                break
+                            view_scenic_score += 1
+                            if self.trees[y][current_x] >= max_tree:
+                                max_tree = max(self.trees[y][current_x], max_tree)
+                                if max_tree >= self.trees[y][x]:
+                                    break
+                    elif move_y != 0:
+                        for j in range(1, len(self.trees)):
+                            current_y += move_y
+                            if current_y < 0 or current_y >= len(self.trees):
+                                break
+                            view_scenic_score += 1
+                            if self.trees[current_y][x] >= max_tree:
+                                max_tree = max(self.trees[current_y][x], max_tree)
+                                if max_tree >= self.trees[y][x]:
+                                    break
+                    if view_scenic_score != 0:
+                        if scenic_score == 0:
+                            scenic_score = 1
+                        scenic_score *= view_scenic_score
+
+                self.scenic_scores[y][x] = scenic_score
+                max_scenic_score = max(max_scenic_score, scenic_score)
+        
+        for row in self.scenic_scores:
+            print(row)
+        
+        print(f"Max scenic score {max_scenic_score}")
 
 def get_file(path, file):
     file = os.path.join(path, f"Task Inputs", file)
@@ -86,3 +133,4 @@ if __name__ == "__main__":
 
     treetops = Treetops(get_file(path, input_text))
     treetops.find_visible_trees()
+    treetops.find_scenic_score()
