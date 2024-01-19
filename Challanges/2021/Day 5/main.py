7781b7769541c8e8c0445c44909b5186eacea808
difuria <69213222+difuria@users.noreply.github.com>
2024-01-19 16:31:44 +0000

    Add task 2 solution

--- Challanges/2021/Day 5/main.py
+++ Challanges/2021/Day 5/main.py
@@ -15,17 +15,37 @@ class HydrothermalVents:
         
         self.grid = []
         for y in range(self.largest["y"] + 1):
-            self.grid.append(["."][:] * (self.largest["x"] + 1))
+            self.grid.append(["."] * (self.largest["x"] + 1))
 
-    def draw_straight_lines(self, print_output = True):
+    def draw_straight_lines(self, print_output = True, diagonal_lines = False):
         overlapping_points = 0
         for segment in self.line_segments:
             start, end = segment
-            if not (start[0] == end[0] or start[1] == end[1]):
-                # This isn't a straight line
+
+            x_diff = abs(start[0] - end[0])
+            y_diff = abs(start[1] - end[1])
+            if not (start[0] == end[0] or start[1] == end[1]) and not (diagonal_lines and x_diff == y_diff):
                 continue
 
-            if start[1] != end[1]:
+            if diagonal_lines and x_diff == y_diff:
+                x_movement = -1 if start[0] > end[0] else 1
+                y_movement = -1 if start[1] > end[1] else 1
+                x_start, x_end = start
+                x_current, y_current = start
+                x_end, y_end = end
+                while True:
+                    if self.grid[y_current][x_current] == ".":
+                        self.grid[y_current][x_current] = 0
+                    elif self.grid[y_current][x_current] == 1:
+                        overlapping_points += 1
+                    self.grid[y_current][x_current] += 1
+
+                    x_current += x_movement
+                    y_current += y_movement
+
+                    if (x_start < x_end and x_current > x_end) or (x_start > x_end and x_current < x_end):
+                        break
+            elif start[1] != end[1]:
                 x = start[0]
                 movement = -1 if start[1] > end[1] else 1
                 for y in range(start[1], end[1] + movement, movement):
@@ -44,11 +64,13 @@ class HydrothermalVents:
                         overlapping_points += 1
                     self.grid[y][x] += 1
         
-        if print_output:
-            for line in self.grid:
-                output = ""
-                for item in line:
-                    output += str(item)
+        other_count = 0
+        
+        for line in self.grid:
+            output = ""
+            for item in line:
+                output += str(item)
+            if print_output:
                 print(output)
         
         print(f"There were {overlapping_points} overlapping points.")
@@ -71,7 +93,11 @@ if __name__ == "__main__":
     print("Test Inputs")
     hydrothermal_vents = HydrothermalVents(get_file(path, "test_vents_1.txt"))
     hydrothermal_vents.draw_straight_lines()
+    hydrothermal_vents = HydrothermalVents(get_file(path, "test_vents_1.txt"))
+    hydrothermal_vents.draw_straight_lines(True, True)
 
     print("\nPuzzle Inputs")
     hydrothermal_vents = HydrothermalVents(get_file(path, "puzzle_vents_1.txt"))
     hydrothermal_vents.draw_straight_lines(False)
+    hydrothermal_vents = HydrothermalVents(get_file(path, "puzzle_vents_1.txt"))
+    hydrothermal_vents.draw_straight_lines(False, True)
