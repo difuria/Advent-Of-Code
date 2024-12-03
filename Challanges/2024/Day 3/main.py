72e5726f1112d28912b5c1fa82d3744e49048dbd
difuria <69213222+difuria@users.noreply.github.com>
2024-12-03 10:41:23 +0000

    Simplify tasks into a single function

--- Challanges/2024/Day 3/main.py
+++ Challanges/2024/Day 3/main.py
@@ -3,25 +3,8 @@ from Inputs.test import test_input_1, test_input_2
 
 import re
 
-def get_total(puzzle_input):
-    muls = re.findall(r'mul\([0-9{1,},[0-9]{1,}\)', puzzle_input)
-    total = 0
-    for mul in muls:
-        mul = mul.replace("mul(", "").replace(")", "")
-        try:
-            x, y = list(map(int, mul.split(",")))
-            total += (x*y)
-        except:
-            # Ignore single finds such as mul(901)
-            pass
-    
-    return total
-
-print(f"Total for test input is {get_total(test_input_1)}")
-print(f"Total for puzzle input is {get_total(puzzle_input_1)}")
-
-def get_total_2(puzzle_input):
-    muls = re.findall(r'(mul\([0-9{1,},[0-9]{1,}\)|don\'t\(\)|do\(\))', puzzle_input)
+def get_total(puzzle_input, regex):
+    muls = re.findall(regex, puzzle_input)
     total = 0
     do = True
     for mul in muls:
@@ -44,6 +27,11 @@ def get_total_2(puzzle_input):
     
     return total
 
-print(f"Total for task 2 test input 1 is {get_total_2(test_input_1)}")
-print(f"Total for task 2 test input 2 is {get_total_2(test_input_2)}")
-print(f"Total for task 2 puzzle input is {get_total_2(puzzle_input_1)}")
+regex = r'mul\([0-9{1,},[0-9]{1,}\)'
+print(f"Total for test input is {get_total(test_input_1, regex)}")
+print(f"Total for puzzle input is {get_total(puzzle_input_1, regex)}")
+
+regex = r'(mul\([0-9{1,},[0-9]{1,}\)|don\'t\(\)|do\(\))'
+print(f"Total for task 2 test input 1 is {get_total(test_input_1, regex)}")
+print(f"Total for task 2 test input 2 is {get_total(test_input_2, regex)}")
+print(f"Total for task 2 puzzle input is {get_total(puzzle_input_1, regex)}")
