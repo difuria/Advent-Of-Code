fc079ba6f54d92730756c2652c8f2feae166e241
difuria <69213222+difuria@users.noreply.github.com>
2024-12-03 14:36:44 +0000

    Update main.py

--- Challanges/2024/Day 3/main.py
+++ Challanges/2024/Day 3/main.py
@@ -23,11 +23,11 @@ def get_total(puzzle_input, regex):
     
     return total
 
-regex = r'mul\([0-9]{1,},[0-9]{1,}\)'
+regex = r'mul\(\d{1,},\d{1,}\)'
 print(f"Total for test input is {get_total(test_input_1, regex)}")
 print(f"Total for puzzle input is {get_total(puzzle_input_1, regex)}")
 
-regex = r'(mul\([0-9]{1,},[0-9]{1,}\)|don\'t\(\)|do\(\))'
+regex = r'(mul\(\d{1,},\d{1,}\)|don\'t\(\)|do\(\))'
 print(f"Total for task 2 test input 1 is {get_total(test_input_1, regex)}")
 print(f"Total for task 2 test input 2 is {get_total(test_input_2, regex)}")
 print(f"Total for task 2 puzzle input is {get_total(puzzle_input_1, regex)}")
