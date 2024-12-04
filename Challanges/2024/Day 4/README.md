b9ff66ea6e8cf1e2876b40ad906fbade92ddee4d
difuria <69213222+difuria@users.noreply.github.com>
2024-12-04 11:10:44 +0000

    Day 4 task 2

--- Challanges/2024/Day 4/README.md
+++ Challanges/2024/Day 4/README.md
@@ -73,3 +73,7 @@ Here's the same example from before, but this time all of the `X-MAS`es have bee
 In this example, an `X-MAS` appears **`9`** times.
 
 Flip the word search from the instructions back over to the word search side and try again. **How many times does an** **`X-MAS`** **appear?**
+
+Your puzzle answer was `2046`.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
--- Challanges/2024/Day 4/main.py
+++ Challanges/2024/Day 4/main.py
@@ -21,6 +21,7 @@ def breadth_first_search(text: list, desired_word: str, i: int, j: int) -> list:
             elif desired_word[1] == text[new_y][new_x]:
                 # current position
                 # moving position
+                # current index
                 # Current path for debugging
                 queue.append([
                     [new_x, new_y],
@@ -94,9 +95,67 @@ def print_map(found: list, text:str) -> None:
     print(new_text)
 
 
-text = test_input_1.split("\n")
-found = find_starting_letter(text)
-print_map(found, text)
+def corner_in_text(text:str, corners: list):
+    for corner in corners:
+        x, y = corner
+        
+        if y >= len(text) or y < 0:
+            return False
+        elif x >= len(text[y]) or x < 0:
+            return False
+    
+    return True
 
-text = puzzle_input_1.split("\n")
-found = find_starting_letter(text)
+
+def x_mas_map(text: list):
+    found = []
+    for y, line in enumerate(text):
+        for x, letter in enumerate(line):
+            if letter == "A":
+                find = [["M", "S"], ["M", "S"]]
+                top_left_corner = [x - 1, y + 1]
+                bottom_left_corner = [x - 1, y -1]
+                top_right_corner = [x + 1, y + 1]
+                bottom_right_corner = [x + 1, y - 1]
+
+                if not corner_in_text(text, [top_left_corner, bottom_left_corner, top_right_corner, bottom_right_corner]):
+                    continue
+
+                t_l_x, t_l_y = top_left_corner
+                b_l_x, b_l_y = bottom_left_corner
+                t_r_x, t_r_y = top_right_corner
+                b_r_x, b_r_y = bottom_right_corner
+
+                if text[t_l_y][t_l_x] in find[0]:
+                    find[0].remove(text[t_l_y][t_l_x])
+                    if text[b_r_y][b_r_x] != find[0][0]:
+                        continue
+                else:
+                    continue
+
+                if text[t_r_y][t_r_x] in find[1]:
+                    find[1].remove(text[t_r_y][t_r_x])
+                    if text[b_l_y][b_l_x] != find[1][0]:
+                        continue
+                else:
+                    continue
+
+                found.append([[x, y], top_left_corner, bottom_right_corner, top_right_corner, bottom_left_corner])
+    
+    print(f"We've found {len(found)} X-MASes")
+    return found
+
+
+test_text = test_input_1.split("\n")
+puzzle_text = puzzle_input_1.split("\n")
+
+print("Task 1")
+task_1_test_found = find_starting_letter(test_text)
+print_map(task_1_test_found, test_text)
+task_1_puzzle_found = find_starting_letter(puzzle_text)
+
+
+print("\nTask 2")
+found = x_mas_map(test_text)
+print_map(found, test_text)
+x_mas_map(puzzle_text)
