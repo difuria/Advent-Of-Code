11e7b91b4a175299468e99626c48826b9bf8639a
difuria <69213222+difuria@users.noreply.github.com>
2024-12-16 21:54:52 +0000

    Backup
    This works for the examples, but not for the puzzle  input as it's removing

--- Challanges/2024/Day 16/main.py
+++ Challanges/2024/Day 16/main.py
@@ -5,18 +5,20 @@ import sys
 
 class ReindeerMaze:
     def __init__(self, text: str) -> None:
-        self.grid = self._get_grid(text)
+        self.grid, self.og = self._get_grid(text)
         self.start, self.end = self.find_start()
 
         self.completed_paths = []
 
     def _get_grid(self, text: str) -> list[list[str]]:
         grid = []
+        og = []
 
         for line in text.strip().splitlines():
             grid.append(list(line.strip()))
+            og.append(list(line.strip()))
         
-        return grid
+        return grid, og
     
     def find_start(self) -> tuple[int]:
         start = None
@@ -59,10 +61,7 @@ class ReindeerMaze:
                 new_x = item["position"][0] + movement[0]
                 new_y = item["position"][1] + movement[1]
                 new_pos = (new_x, new_y)
-                turn_score = abs(i - item["direction"])
-                if turn_score == 3:
-                    turn_score = 1
-                
+                turn_score = abs(i - item["direction"]) % 2  # Will only be a single turn or no turn
                 new_score = item["score"] + 1 + (1000 * turn_score)
 
                 position = {
@@ -87,17 +86,89 @@ class ReindeerMaze:
                 if new_pos == self.end:
                     min_score = min(min_score, position["score"])
 
+        print("Score", self.grid[7][3])
         print(f"Minimum score to the end is {min_score}.")
 
+    def find_tiles_in_shorest_path(self):
+        queue = [{
+            "position": self.end, 
+            "path": {self.end}
+        }]
 
-print("Task 1")
+        movements = (
+            (1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1)
+        )
+
+        shorted_path_tiles = set()
+        # We need to ensure we reach the starting position again
+        while queue:
+            item = queue.pop(0)
+            x, y = item["position"]
+            cur_score = self.grid[y][x]["score"]
+
+            for movement in movements:
+                new_x = x + movement[0]
+                new_y = y + movement[1]
+                new_pos = (new_x, new_y)
+
+                if self.grid[new_y][new_x] == "#":
+                    continue
+                elif new_y < 0 or new_y > len(self.grid):
+                    continue
+                elif new_x < 0 or new_x > len(self.grid[new_y]):
+                    continue
+
+                new_score = self.grid[new_y][new_x]["score"]
+
+                potential_scores = [
+                    cur_score - 1,
+                    cur_score - 1 - 1000
+                ]
+
+                # From the end posiion we should only be going down
+                if (x, y) != self.end:
+                    potential_scores.append(cur_score - 1 + 1000) # Fewer turns could be used at a later time)
+
+                if new_score in potential_scores:
+                    new_path = item["path"].copy()
+                    new_path.add(new_pos)
+                    new_item = {
+                        "path": new_path,
+                        "position": (new_x, new_y)
+                    }
+                    
+                    if new_score == 0:
+                        # We're at the end
+                        shorted_path_tiles.update(new_item["path"])
+                    else:
+                        queue.append(new_item)
+
+        print(f"\nThere are {len(shorted_path_tiles)} tiles that can be in the shortest path.")
+
+        for y, line in enumerate(self.grid):
+            p_line = ""
+            for x, char in enumerate(line):
+                if (x,y) in shorted_path_tiles:
+                    p_line += "O"
+                else:
+                    p_line += self.og[y][x]
+            print(p_line)
+
 print("Test Inputs")
 ti1 = ReindeerMaze(test_input_1)
 ti1.find_paths()
+ti1.find_tiles_in_shorest_path()
 
 ti2 = ReindeerMaze(test_input_2)
 ti2.find_paths()
+ti2.find_tiles_in_shorest_path()
 
 print("Puzzle Input")
-p1 = ReindeerMaze(puzzle_input_1)
+p1 = ReindeerMaze(test_input_1)
 p1.find_paths()
+p1.find_tiles_in_shorest_path()
+
+# 703 is too high
