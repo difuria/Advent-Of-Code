0b17324852588d11824571acb2461038381dc956
difuria <69213222+difuria@users.noreply.github.com>
2024-12-17 16:46:05 +0000

    2024 Day 17 task 1 solution

--- Challanges/2024/Day 17/main.py
+++ Challanges/2024/Day 17/main.py
@@ -32,65 +32,60 @@ def get_combo_operand(debugger: dict[str, int], operand: int) -> int:
         return combo_operand.get(operand, operand)
 
 
-def dv(debugger: dict[str, int], operand: int) -> int:
+def division(debugger: dict[str, int], operand: int) -> int:
     numerator = debugger["A"]
     denominator = 2**get_combo_operand(debugger, operand)
     return int(numerator / denominator)
 
-
-def execute_opcode(opcode: int, operand: int, debugger: dict[str, int]) -> dict[str, int]:
-    if opcode == 0:
-        # adv
-        debugger["A"] = dv(debugger, operand)
-    elif opcode == 1:
-        # bxl
-        debugger["B"] = debugger["B"] ^ operand
-    elif opcode == 2:
-        # bst
-        modulo = get_combo_operand(debugger, operand) % 8
-        print("Modulo", modulo)
-        debugger["B"] = get_combo_operand(debugger, operand) % 8
-    elif opcode == 3:
-        # jnz
-        if debugger["A"] == 0:
-            # Do nothing
-            return debugger
-        # Needs to recursively call
-        return execute_opcode(operand, operand, debugger)
-    elif opcode == 4:
-        # bxc
-        debugger["B"] =  debugger["B"] ^ debugger["C"]
-    elif opcode == 5:
-        # out
-        modulo = get_combo_operand(debugger, operand) % 8
-        debugger["out"] += list(str(modulo))
-    elif opcode == 6:
-        # bdv
-        debugger["B"] = dv(debugger, operand)
-    elif opcode == 7:
-        # cdv
-        debugger["C"] = dv(debugger, operand)
-    else:
-        raise ValueError(f"Invalid opcode of {opcode} supplied.")
-
-    return debugger
-
-
 def run_program(debugger: dict[str, int]) -> None:
-    for i in range(0, len(debugger["Program"]), 2):
-        opcode = debugger["Program"][i]
-        operand = debugger["Program"][i+1]
+    pointer = 0
+    while pointer < len(debugger["Program"]):
+        opcode = debugger["Program"][pointer]
+        operand = debugger["Program"][pointer+1]
 
-        debugger = execute_opcode(opcode, operand, debugger)
+        if opcode == 0:
+            # adv
+            debugger["A"] = division(debugger, operand)
+        elif opcode == 1:
+            # bxl
+            debugger["B"] = debugger["B"] ^ operand
+        elif opcode == 2:
+            # bst
+            debugger["B"] = get_combo_operand(debugger, operand) % 8
+        elif opcode == 3:
+            # jnz
+            if debugger["A"] != 0:
+                # Do nothing
+                pointer = operand
+                continue
+        elif opcode == 4:
+            # bxc
+            debugger["B"] =  debugger["B"] ^ debugger["C"]
+        elif opcode == 5:
+            # out
+            modulo = get_combo_operand(debugger, operand) % 8
+            debugger["out"] += list(str(modulo))
+        elif opcode == 6:
+            # bdv
+            debugger["B"] = division(debugger, operand)
+        elif opcode == 7:
+            # cdv
+            debugger["C"] = division(debugger, operand)
+        else:
+            raise ValueError(f"Invalid opcode of {opcode} supplied.")
+        
+        pointer += 2
     
-    print(debugger)
-    input()
     return debugger
 
 
-_debugger = get_debugger(puzzle_input_1)
-print(_debugger)
-while len(_debugger["out"]) == 0 or _debugger["out"][-1] != '0':
-    _debugger = run_program(_debugger)
+print("Task 1")
+print("Test Input")
+_debugger = get_debugger(test_input_1)
+_debugger = run_program(_debugger)
+print("Output: " + ",".join(_debugger["out"]))
 
-print("output: " + ",".join(_debugger["out"]))
+print("\nPuzzle Input")
+_debugger = get_debugger(puzzle_input_1)
+_debugger = run_program(_debugger)
+print("Output: " + ",".join(_debugger["out"]))
