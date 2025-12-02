3104247276cea4a9111ef93f3ae61d311b3514d0
difuria <69213222+difuria@users.noreply.github.com>
2025-12-02 08:17:35 +0000

    More efficient part 1 solution

--- Challanges/2025/Day 1/main.py
+++ Challanges/2025/Day 1/main.py
@@ -1,28 +1,32 @@
 from Inputs.test import test1
 from Inputs.puzzle import puzzle1
 
-def read_combination(combinations: str, start: int = 50):
+def read_combination(combinations: str, start: int = 50) -> int:
     combos: list[str] = combinations.strip().splitlines()
     current: int = start
     zero_count: int = 0
+
     for combo in combos:
         direction: str = combo[0]
         value: int = int(combo[1:])
 
         if direction == "L":
             current -= value
-            while current < 0:
-                current += 100
         else:
             current += value
-            while current > 99:
-                current -= 100
+
+        if current < 0:
+            if (abs(current) % 100) == 0:
+                current = 0
+            else:
+                current = 100 - (abs(current) % 100)
+        elif current > 99:
+            current = 0 + (abs(current) % 100)
 
         if current == 0:
             zero_count += 1
 
     return zero_count
-
 
 print("Part 1")
 print(f"Test 0's: {read_combination(test1)}")
