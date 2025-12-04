cf4c9dce9106709c2ae213f382d3eece9a4ac89e
difuria <69213222+difuria@users.noreply.github.com>
2025-12-04 14:30:57 +0000

    2025 Day 4 solutions

---
+++ Challanges/2025/Day 4/main.py
@@ -0,0 +1,90 @@
+from Inputs.test import t1
+from Inputs.puzzle import p1
+
+
+def find_paper(movement: tuple[int], row:int, col:int, locations: list[str]) -> int:
+    row += movement[0]
+    col += movement[1]
+
+    
+    if row < 0 or row >= len(locations): 
+        return 0
+    elif col < 0 or col >= len(locations[row]):
+        return 0
+    elif locations[row][col] == ".":
+        return 0
+    else:
+        return 1
+
+
+def locate_paper(locations: str, max_adjacet_rolls: int = 3, output: bool = False) -> tuple[int, list[list[str]]]:
+    og: list[list[str]] = []
+    map: list[list[str]] = []
+    for row in locations.strip().splitlines():
+        map.append([])
+        og.append([])
+        for col in row:
+                col = "." if col == "x" else col
+                map[-1].append(col)
+                og[-1].append(col)
+
+    movements: tuple[tuple[int]] = (
+        (1, 0),
+        (-1, 0),
+        (0, 1),
+        (0, -1),
+        (1, 1),
+        (1, -1),
+        (-1, 1),
+        (-1, -1),
+    )
+
+    count: int = 0
+    exits: int = 0 # temp
+    for y in range(len(map)):
+        for x in range(len(map[y])):
+            paper: str = map[y][x]
+
+            if paper != "@":
+                exits += 1
+                continue
+
+            adjacent_paper_count: int = 0
+            for movement in movements:
+                adjacent_paper_count += find_paper(movement, y, x, og)
+
+            if adjacent_paper_count <= max_adjacet_rolls:
+                map[y][x] = "x"
+                count += 1
+
+    out: str = ""
+    for row in map:
+        out += "".join(row).strip() + "\n"
+    out = out.strip()
+
+    if output:
+        print(out + "\n")
+
+    return count, out
+
+
+def determine_total_to_remove(map: str, output: bool = False) -> int:
+    old_map: int = ""
+    total: int = 0
+    current_map: str = map.strip()
+    while old_map != current_map:
+        old_map = current_map
+
+        moved, current_map = locate_paper(current_map, output=output)
+        total += moved
+    
+    return total
+
+
+print("Task 1")
+print(f"For the test input there are {locate_paper(t1, output=True)[0]} rolls of paper that can be moved")
+print(f"For the puzzle input there are {locate_paper(p1)[0]} rolls of paper that can be moved")
+
+print("\nTask 2")
+print(f"For the test input there are a {determine_total_to_remove(t1)} total of rolls of paper that can be moved")
+print(f"For the puzzle input there are a {determine_total_to_remove(p1)} total of rolls of paper that can be moved")
